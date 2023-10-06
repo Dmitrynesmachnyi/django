3f8f71e15f14a2f74058d64a227019f7da28f36b
nesma <123456789>
2023-10-06 08:53:09 +0300

    7 занятие 4 модуля + дз

--- advertisements/advertisements/settings.py
+++ advertisements/advertisements/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app_advertisements',
     'app_lesson_4',
+    'app_auth',
 ]
 
 MIDDLEWARE = [
@@ -45,7 +46,7 @@ ROOT_URLCONF = 'advertisements.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [Path.joinpath(BASE_DIR, 'templates')],
+        'DIRS': [Path.joinpath(BASE_DIR)],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- advertisements/app_advertisements/views.py
+++ advertisements/app_advertisements/views.py
@@ -7,23 +7,23 @@ from .forms import AdvertisementForm
 def index(request):
     advertisements = Advertisement.objects.all()
     context = {'advertisements': advertisements}
-    return render(request, 'index.html', context)
+    return render(request, 'app_advertisements/index.html', context)
 
 def top_sellers(request):
-    return render(request, 'top-sellers.html')
+    return render(request, 'app_advertisements/top-sellers.html')
 
 def advertisement_post(request):
     if request.method == "POST":
         form = AdvertisementForm(request.POST, request.FILES)
         if form.is_valid():
-            advertisement = Advertisement(**form.cleaned_data)
-            advertisement.user = request.user
-            advertisement.save()
+            new_advertisement = form.save(commit=False)
+            new_advertisement.user = request.user
+            new_advertisement.save()
             url = reverse('main-page')
             return redirect(url)
     else:
         form = AdvertisementForm()
     context = {'form': form}
-    return render(request, 'advertisement-post.html',context)
+    return render(request, 'app_advertisements/advertisement-post.html',context)
 
 # Create your views here.
